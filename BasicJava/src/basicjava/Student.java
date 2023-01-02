e089291d58d36bc0207231ba1f8bcd48cf44bcb7
raonak-jahan <mimiraonak1998@gmail.com>
2023-01-02 17:32:17 +0600

    Added more files

---
+++ BasicJava/src/basicjava/Student.java
@@ -0,0 +1,19 @@
+package basicjava;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		//Use of Escape sequence \n
+		
+		int id = 11;
+		String name = "Shayan";
+		String sibling = "Five";
+		String address = "Dhaka, Bangladesh";
+		String group = "Science";
+		
+		System.out.println("ID:"+id+"\n"+"Name:"+name+"\n"+"Sibling:"+sibling+"\n"+"Address:"+address+"\n"+"Group:"+group);
+		
+	}
+
+}
---
+++ BasicJava/src/basicjava/UseofEscapeSequence.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+public class UseofEscapeSequence {
+	
+	public static void main(String[] args) {
+		//\n used to print new line
+		System.out.println("Raonak Jahan \n017223344 \nBsc in CSE");
+		//\t used to print a tab
+		System.out.println("1 \t 2");
+		// \" used to print double quotation
+		System.out.println("\"Java Programming\" ");
+		
+	}
+
+}
