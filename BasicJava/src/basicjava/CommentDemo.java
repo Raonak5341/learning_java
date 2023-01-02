bc2e7a13dca89566ddf317abee86eb01564ee773
raonak-jahan <mimiraonak1998@gmail.com>
2023-01-02 20:12:47 +0600

    added data

---
+++ BasicJava/src/basicjava/CommentDemo.java
@@ -0,0 +1,23 @@
+/* 
+ * Author: Raonak Jahan Mimi
+ * Qualification: B.Sc.(Engg)
+ * 02/01/2023
+ */
+
+
+
+
+package basicjava;
+
+public class CommentDemo {
+	
+	public static void main(String[] args) {
+		
+		//Displaying my name
+		System.out.println("Raonak Jahan");
+		
+		//Displaying my hobby
+		System.out.println("My hobby is Travelling");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-## JAVA Learning
+# JAVA Learning
 This repository contains basic java codes, i would upload my daily learning codes and files in it.
+
+
+## JAVA Program Execution Process
+
+<img src="./images/javaProcess.jpg" width="600" title="Java Execution Process"/>
+
