18e2ed96a6b1cd277dab4f74541526d10ebec4d6
raonak-jahan <mimiraonak1998@gmail.com>
2023-01-03 21:16:15 +0600

    created another class

---
+++ BasicJava/src/basicjava/Assignment2.java
@@ -0,0 +1,17 @@
+package basicjava;
+
+public class Assignment2 {
+	
+	public static void main(String[] args) {
+		
+		int id = 101;
+		String title = "iphone15";
+		String price = "1895 euros";
+		String description = "perfect product with best image quality";
+		String category = "phone";
+		
+		System.out.println("ID: "+id+"\n"+"Title: "+title+"\n"+"Price: "+price+"\n"+"Description: "+description+"\n"+"Category: "+category);
+		
+	}
+
+}
---
+++ BasicJava/src/basicjava/datatype2.java
@@ -0,0 +1,24 @@
+package basicjava;
+
+public class datatype2 {
+	
+	public static void main(String[] args) {
+		
+		boolean b = true; //dynamic initialisation
+		char c = 'a';
+		int i = 1234;
+		float g = 10.2f;
+		double d = 10.2;
+		short s = 32677;
+		
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+		System.out.println("i = "+i);
+		System.out.println("g = "+g);
+		System.out.println("d = "+d);
+		System.out.println("s = "+s);
+		
+		
+	}
+
+}
